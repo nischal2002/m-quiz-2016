2bd601f8ecf3e292d0235015f3c31617ae6e26d3
nischal2002 <nischal2002@gmail.com>
2015-12-28 19:48:43 +0530

    Refactored classOne functionality into classOne.py

---
+++ classOne.py
@@ -0,0 +1,103 @@
+import random # This allows you to generate random numbers
+import select # This allows you to wait for a specific time for input from user
+import sys    # this allows you to read the user input from keyboard also called "stdin"
+
+TIMEOUT=10  # this is the amount of time you will wait for an answer in Seconds. 10 means 10 seconds
+QUESTION_COUNT=10
+ANSWER_GOAL=8
+
+
+def classOneQuiz(): # Class 1
+    '''this will the Quiz for CLASS 1'''
+    questionCount = 0
+    correctAnswers = 0
+    while(questionCount < QUESTION_COUNT):
+        if (additionQuestionClassOne()) :
+            correctAnswers = correctAnswers + 1;
+        questionCount = questionCount + 1
+    print("You got " + str(correctAnswers) + " correct");
+    if(correctAnswers >= ANSWER_GOAL):
+        print("congratulations!,you win!!!")
+    else :
+        print("sorry,better luck next time:)")
+          
+
+
+
+
+def additionQuestionClassOne(): #class 1
+    ''' This will ask a addition question and will wait for user 
+        input and will validate and input. It will terurn boolean true or false
+        based on if the user input is correct or wrong '''
+    if (random.randrange(0,2) == 1):
+        a = getRandomSingleDigitInt()
+        b = getRandomSingleDigitInt()
+        return additionQuestion(a,b)
+    else: 
+        a = getRandomSingleDigitInt()    
+        b = getRandomSingleDigitIntBelow(a)
+        return subtractionQuestion(a,b)
+
+
+def getRandomSingleDigitInt(): #class 1
+    ''' return a random single digit positive integer '''
+    return random.randrange(0,10)  # this returns a single digit random number(integer). Number in python means decimal number
+
+
+def getRandomSingleDigitIntBelow(x): #class 1
+    ''' return a random single digit positive integer '''
+    if x != 0:
+        return random.randrange(0,x+1)  # this returns a single digit random number(integer). Number in python means decimal number
+    else :
+        return 0
+
+
+def additionQuestion(a,b): #class 1
+    ''' This will ask a addition question and will wait for user 
+        input and will validate and input. It will terurn boolean true or false
+        based on if the user input is correct or wrong '''
+
+    print("You have "+str(TIMEOUT)+" seconds to answer this question")
+    print(str(a) + " + " + str(b) + " is : ")
+    answerReady, _, _ = select.select([sys.stdin],[],[],TIMEOUT)
+    # above line says that read from stdin and wait for TIMEOUT seconds 
+    # If no response for TIMEOUT seconds, then retun false
+    if answerReady : # if the answer has been input within the TIMEOUT seconds
+        #Let us read the answer and then calculate and see if the answer is correct  
+        try:
+            answer = int(sys.stdin.readline().strip()) # readline() reads the answer and strip() removes the enter key
+            if (answer == (a+b)): # Here we are checking if the answer is correct or wrong
+                return True
+            else :
+                return False
+        except: # If you return a non number, the code will come here
+            return False
+    else:
+        print(" Sorry you have run out of Time to answer the question")
+        return False
+
+
+
+def subtractionQuestion(a,b): #class 1
+    ''' This will ask a substraction question and will wait for user 
+        input and will validate and input. It will terurn boolean true or false
+        based on if the user input is correct or wrong '''
+
+    print("You have "+str(TIMEOUT)+" seconds to answer this question")
+    print(str(a) + " - " + str(b) + " is : ")
+    answerReady, _, _ = select.select([sys.stdin],[],[],TIMEOUT)
+    # above line says that read from stdin and wait for TIMEOUT seconds 
+    # If no response for TIMEOUT seconds, then retun false
+    if answerReady : # if the answer has been input within the TIMEOUT seconds
+        #Let us read the answer and then calculate and see if the answer is correct  
+        try:
+            answer = int(sys.stdin.readline().strip()) # readline() reads the answer and strip() removes the enter key
+            if (answer == (a-b)): # Here we are checking if the answer is correct or wrong
+                return True
+            else :
+                return False
+        except: # If you return a non number, the code will come here
+            return False
+    else:
+        print(" Sorry you have run out of Time to answer the question")
+        return False
--- quiz.py
+++ quiz.py
@@ -1,6 +1,5 @@
-import random # This allows you to generate random numbers
-import select # This allows you to wait for a specific time for input from user
 import sys    # this allows you to read the user input from keyboard also called "stdin"
+import classOne # This imports all the classOne functions
 
 
 TIMEOUT=10  # this is the amount of time you will wait for an answer in Seconds. 10 means 10 seconds
@@ -9,106 +8,11 @@ QUIZ_INSTRUCTIONS = """
 Get ready for the quiz. You will have 10 questions out of which you 
 will need 8 right to win the prize. You will have """ + str(TIMEOUT) +  """ seconds
 to answer each question.Press Enter to start."""
-QUESTION_COUNT=10
-ANSWER_GOAL=8
 
 
 
-def classOneQuiz():
-    '''this will the Quiz for CLASS 1'''
-    questionCount = 0
-    correctAnswers = 0
-    while(questionCount < QUESTION_COUNT):
-        if (additionQuestionClassOne()) :
-            correctAnswers = correctAnswers + 1;
-        questionCount = questionCount + 1
-    print("You got " + str(correctAnswers) + " correct");
-    if(correctAnswers >= ANSWER_GOAL):
-        print("congratulations!,you win!!!")
-    else :
-        print("sorry,better luck next time:)")
-          
-              
 
-
-def additionQuestionClassOne():
-    ''' This will ask a addition question and will wait for user 
-        input and will validate and input. It will terurn boolean true or false
-        based on if the user input is correct or wrong '''
-    if (random.randrange(0,2) == 1):
-        a = getRandomSingleDigitInt()
-        b = getRandomSingleDigitInt()
-        return additionQuestion(a,b)
-    else: 
-        a = getRandomSingleDigitInt()    
-        b = getRandomSingleDigitIntBelow(a)
-        return subtractionQuestion(a,b)
-
-
-
-def getRandomSingleDigitInt():
-    ''' return a random single digit positive integer '''
-    return random.randrange(0,10)  # this returns a single digit random number(integer). Number in python means decimal number
-
-
-def getRandomSingleDigitIntBelow(x):
-    ''' return a random single digit positive integer '''
-    if x != 0:
-        return random.randrange(0,x+1)  # this returns a single digit random number(integer). Number in python means decimal number
-    else :
-        return 0
-
-def additionQuestion(a,b):
-    ''' This will ask a addition question and will wait for user 
-        input and will validate and input. It will terurn boolean true or false
-        based on if the user input is correct or wrong '''
-
-    print("You have "+str(TIMEOUT)+" seconds to answer this question")
-    print(str(a) + " + " + str(b) + " is : ")
-    answerReady, _, _ = select.select([sys.stdin],[],[],TIMEOUT)
-    # above line says that read from stdin and wait for TIMEOUT seconds 
-    # If no response for TIMEOUT seconds, then retun false
-    if answerReady : # if the answer has been input within the TIMEOUT seconds
-        #Let us read the answer and then calculate and see if the answer is correct  
-        try:
-            answer = int(sys.stdin.readline().strip()) # readline() reads the answer and strip() removes the enter key
-            if (answer == (a+b)): # Here we are checking if the answer is correct or wrong
-                return True
-            else :
-                return False
-        except: # If you return a non number, the code will come here
-            return False
-    else:
-        print(" Sorry you have run out of Time to answer the question")
-        return False
-
-
-
-def subtractionQuestion(a,b):
-    ''' This will ask a substraction question and will wait for user 
-        input and will validate and input. It will terurn boolean true or false
-        based on if the user input is correct or wrong '''
-
-    print("You have "+str(TIMEOUT)+" seconds to answer this question")
-    print(str(a) + " - " + str(b) + " is : ")
-    answerReady, _, _ = select.select([sys.stdin],[],[],TIMEOUT)
-    # above line says that read from stdin and wait for TIMEOUT seconds 
-    # If no response for TIMEOUT seconds, then retun false
-    if answerReady : # if the answer has been input within the TIMEOUT seconds
-        #Let us read the answer and then calculate and see if the answer is correct  
-        try:
-            answer = int(sys.stdin.readline().strip()) # readline() reads the answer and strip() removes the enter key
-            if (answer == (a-b)): # Here we are checking if the answer is correct or wrong
-                return True
-            else :
-                return False
-        except: # If you return a non number, the code will come here
-            return False
-    else:
-        print(" Sorry you have run out of Time to answer the question")
-        return False
-
-def getUsersClass():
+def getUsersClass(): #main
     ''' This function will get the user's class. It will compare the class with MAX_CLASS and
     will return False if it is more than the MAX_CLASS. Class also has to be a natural number ''' 
     print("Please tell me which Class you are in? ")
@@ -133,7 +37,7 @@ if __name__ == '__main__':
     print(QUIZ_INSTRUCTIONS)
     sys.stdin.readline()
     if (usersClass == 1) :
-       classOneQuiz()
+       classOne.classOneQuiz()
 
 
 
